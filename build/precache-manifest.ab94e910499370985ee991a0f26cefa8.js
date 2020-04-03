9445c54e352f5282c8f29d4d7d371cf73d219171
Lukasz Strzalka <Lukasz.Strzalka@zooplus.com>
2020-04-03 23:02:29 +0200

    update .gitignore

---
+++ build/precache-manifest.ab94e910499370985ee991a0f26cefa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b587b5aa53d9bfdb9dfb3550bb3ecba0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fae522bb0c74a3dbb286",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e645f71149fb706ef855",
+    "url": "/static/js/2.d7a05046.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7a05046.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fae522bb0c74a3dbb286",
+    "url": "/static/js/main.c5569c2e.chunk.js"
+  },
+  {
+    "revision": "8d8d139b190630b6bc42",
+    "url": "/static/js/runtime-main.7ba82ef7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
