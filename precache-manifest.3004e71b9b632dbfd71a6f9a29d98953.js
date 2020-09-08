0b9256c0f6e5406d47bf3951824a3222d8df20db
Łukasz Strzałka <lukasz.strzalka94@gmail.com>
2020-09-08 20:30:41 +0200

    build new static app

---
+++ precache-manifest.3004e71b9b632dbfd71a6f9a29d98953.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b6ec1b4d39b5a06b03345b0cd1510b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aedbf0d1a012661d647",
+    "url": "/static/css/main.51f92c90.chunk.css"
+  },
+  {
+    "revision": "707b9e543dfa3b8ecf28",
+    "url": "/static/js/2.69f6b70e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.69f6b70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aedbf0d1a012661d647",
+    "url": "/static/js/main.7db4d81e.chunk.js"
+  },
+  {
+    "revision": "8d8d139b190630b6bc42",
+    "url": "/static/js/runtime-main.7ba82ef7.js"
+  },
+  {
+    "revision": "1d415131f27bcad24676ba9236b0d77d",
+    "url": "/static/media/govtech_logo.1d415131.png"
+  },
+  {
+    "revision": "71641cd32482177c458a45693ba9b93d",
+    "url": "/static/media/smartTestLogo.71641cd3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c4f5edb0012d9a3df8506549d08e831.js"
+  "/precache-manifest.3004e71b9b632dbfd71a6f9a29d98953.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
